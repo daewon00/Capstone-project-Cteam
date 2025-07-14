1f8e30da9b7e19569bac5d0685a454ce56528483
highbee123 <dnjstndtjd2@naver.com>
2025-07-14 13:07:41 +0900

    필드 싱글턴 주석 처리
    현 시점 오류가 없긴 한데 card 스크립트에서 instance로 읽지 않기 때문에 필요 없고 필드가 복수이기 때문에 혹시나 헷갈릴 걸 방지해서 주석 처리해서 제거함

--- Mycard/Assets/Scripts/CardPlacePoint.cs
+++ Mycard/Assets/Scripts/CardPlacePoint.cs
@@ -1,12 +1,13 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 필드 위에 카드 놓일 자리 에 대한 정보
 public class CardPlacePoint : MonoBehaviour
 {
-    public static CardPlacePoint instance;
+    //public static CardPlacePoint instance;
 
-    public Card activeCard;
-    public bool isPlayerPoint;
-    public Transform cameraFocusPoint;
+    public Card activeCard; // 놓여있는 카드 정보
+    public bool isPlayerPoint; // 플레이어 영역 참 거짓
+    public Transform cameraFocusPoint; // 카메라 포커스 변수
 }
