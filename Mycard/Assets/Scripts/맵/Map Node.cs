62d79fa0164d665782396b2242de85faa29aa92b
highbee123 <dnjstndtjd2@naver.com>
2025-07-21 20:28:49 +0900

    맵 노드 프리팹 제작 및 사운드
    프리팹 제작해놓고 오디오 매니저 활성시 실행 꺼둠

---
+++ Mycard/Assets/Scripts/맵/Map Node.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapNode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // 전투 노드 버튼이 호출할 함수
+    public void GoToBattleScene()
+    {
+        SceneManager.LoadScene("Battle");
+    }
+
+    // 상점 노드 버튼이 호출할 함수 아직 씬 완성 안됨
+    public void GoToShopScene()
+    {
+        SceneManager.LoadScene("ShopScene");
+    }
+
+    // 이벤트 노드 버튼이 호출할 함수 아직 씬 완성 안됨
+    public void GoToEventScene()
+    {
+        SceneManager.LoadScene("EventScene");
+    }
+}
