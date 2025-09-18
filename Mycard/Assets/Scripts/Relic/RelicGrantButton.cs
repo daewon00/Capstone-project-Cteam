7d0ad4afda8edc48f621d978f43803e2f29fcf09
daewon00 <qkreodnjs12345@gmail.com>
2025-09-18 21:57:10 +0900

    유물확인용 업데이트 플레이어체력 마나 렐릭 작동안함(정상)

--- Mycard/Assets/Scripts/Relic/RelicGrantButton.cs
+++ Mycard/Assets/Scripts/Relic/RelicGrantButton.cs
@@ -4,11 +4,80 @@ using UnityEngine;
 
 public class RelicGrantButton : MonoBehaviour
 {
-    
-    public void AddRelicTest()
+    public GameObject RelicButton;
+
+    public void AddRelicTest1()
     {
+        RelicSystem.Instance.AddRelicById("WarBanner", stacks: 1);
+
+
+    }
+    public void AddRelicTest2()
+    {
+
+        RelicSystem.Instance.AddRelicById("ManaGem", stacks: 1);
+
+    }
+    public void AddRelicTest3()
+    {
+
+        RelicSystem.Instance.AddRelicById("HappyFlower", stacks: 1);
+
+
+    }
+    public void AddRelicTest4()
+    {
+
+        RelicSystem.Instance.AddRelicById("ExtraDraw", stacks: 1);
+
+
+    }
+    public void AddRelicTest5()
+    {
+
+        RelicSystem.Instance.AddRelicById("SheildBanner", stacks: 1);
+
+    }
+    public void AddRelicTest6()
+    {
+
+        RelicSystem.Instance.AddRelicById("ManaDiscount", stacks: 1);
+
+
+    }
+    public void AddRelicTest7()
+    {
+
+        RelicSystem.Instance.AddRelicById("EnemyManaLeech", stacks: 1);
+
+
+    }
+    public void AddRelicTest8()
+    {
+
+        RelicSystem.Instance.AddRelicById("EnemyFirstCardWeakener", stacks: 1);
+
+
+    }
+    public void AddRelicTest9()
+    {
+
         RelicSystem.Instance.AddRelicById("COMP_COMP_Knight", stacks: 1);
+
     }
 
+    public void RelicButtonOn()
+    {
+        if (RelicButton.activeSelf == false)
+        {
+            RelicButton.SetActive(true);
+
+        }
+        else
+        {
+            RelicButton.SetActive(false);
+        }
+        
+    }
 
 }
